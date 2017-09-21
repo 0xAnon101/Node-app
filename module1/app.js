917695b44414a7478e38625ccaba41ff57106641
Raju Gautam <gautamraju15@stu.upes.ac.in>
2017-09-21 10:38:01 -0700

    created a server

--- module1/app.js
+++ module1/app.js
@@ -2,12 +2,30 @@
  * Created by ARMY_BRAT on 19/09/2017.
  */
 var http = require('http');
+var fs = require('fs');
 
-
-function onRequest(request, response) {
-
-    response.writeHead(200, {'Context-type': 'text/plain'});
-    response.write("Hello world");
+//404 response
+function send404Response(response) {
+    response.writeHead(404, {"Context-type": "text/plain"});
+    response.write("Error 404: Page not found");
     response.end();
 }
+
+//handle user request
+function onRequest(request, response) {
+    if (request.method == 'GET' && request.url == "/") {
+
+        response.writeHead(200, {"Context-type": "text/html"});
+        // send a file in stream
+        fs.createReadStream("module1/index.html").pipe(response);
+
+    }
+    else {
+        send404Response(response);
+
+    }
+
+}
+
 http.createServer(onRequest).listen(8081, "127.0.0.1");
+console.log("server running at http://127.0.0.1:8081");
